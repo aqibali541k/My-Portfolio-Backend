801ed23af1d40480aa33a61fc7353c3097bec974
Aqib Shabbir <aqibali541k@gmail.com>
2026-02-01 23:42:18 +0500

    fix delete api

--- index.js
+++ index.js
@@ -92,12 +92,12 @@ app.use("/projects", projectRouter);
 app.use("/contact", contactRouter);
 /* ---------------- ROUTES ---------------- */
 
-// app.get("/", (req, res) => {
-//   res.send("server is online");
-// });
-
-// module.exports = app;
-
-app.listen(process.env.PORT, () => {
-  console.log("Server is running perfectly on PORT:", process.env.PORT);
+app.get("/", (req, res) => {
+  res.send("server is online");
 });
+
+module.exports = app;
+
+// app.listen(process.env.PORT, () => {
+//   console.log("Server is running perfectly on PORT:", process.env.PORT);
+// });
